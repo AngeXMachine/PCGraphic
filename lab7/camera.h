c58faf839cb5ca5cf13897184d61eea85ab1118c
AngeXMachine <116073127+AngeXMachine@users.noreply.github.com>
2023-05-15 21:13:21 +0800

    Delete lab7 directory

--- lab7/camera.h
+++
@@ -1,6 +0,0 @@
-#ifndef CAMERA_H_INCLUDED
-#define CAMERA_H_INCLUDED
-
-void MoveCamera();
-
-#endif // CAMERA_H_INCLUDED
--- lab7/lighting.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIGHT_H_INCLUDED
-#define LIGHT_H_INCLUDED
-
-void Init_Material();
-void Init_Light();
-
-#endif // LIGHT_H_INCLUDED
